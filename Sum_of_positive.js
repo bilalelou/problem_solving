f99b584c9ee22cb71e1dd1b8eba266927fdcc903
Bilal <bilal.12elou@gmail.com>
2026-03-19 22:17:09 +0000

    feat: Add 'Sum of positive' problem file and configure VS Code settings.

---
+++ Sum_of_positive.js
@@ -0,0 +1,32 @@
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example
+// [1, -4, 7, 12] => 
+// 1
+// +
+// 7
+// +
+// 12
+// =
+// 20
+// 1+7+12=20
+// Note
+// If there is nothing to sum, the sum is default to 0.
+
+
+function positiveSum(arr) {
+    // method 1
+    //   if(arr.length === 0){
+    //     return 0;
+    //   }
+    //   let sum = 0;
+    //   for(let i =0;i<arr.length;i++){
+    //     if(arr[i] > 0){
+    //       sum += arr[i];
+    //     }
+    //   }
+    //   return sum;
+
+    // method 2
+    return arr.filter((x) => x > 0).reduce((acc, current) => acc + current, 0)
+}
